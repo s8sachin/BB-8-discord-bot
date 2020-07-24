909f53540561888dbbcfd43bd6140a56fb12fc75
s8sachin <s.sachin94@gmail.com>
2020-07-24 20:44:19 +0530

    url change

--- utils/greetingMessage.js
+++ utils/greetingMessage.js
@@ -1,10 +1,10 @@
 const greetingMessage = `Hello, Thanks for adding me. 👍\n
 I have ability to inform when someone is streaming a game rn.\n\n
-Suggest ideas / contribute code here https://github.com/s8sachin/repo-name\n
+Suggest ideas / contribute code here https://github.com/s8sachin/BB-8-discord-bot\n
 -- developed by https://github.com/s8sachin (Bat)`;
 
 const bb8Info = `I am a bot developed by https://github.com/s8sachin (Bat) \n\n
-Suggest ideas / contribute code here https://github.com/s8sachin/repo-name`;
+Suggest ideas / contribute code here https://github.com/s8sachin/BB-8-discord-bot`;
 
 module.exports = {
   greetingMessage,
