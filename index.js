83364de65c82c5e1f31de2205d94cd89b5bb7fc7
rohithv1992 <rohith.v.1992@gmail.com>
2020-09-03 14:37:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ client.on("voiceStateUpdate", (oldVoiceState, newVoiceState) => {
     console.log(newVoiceState, 'MEMBEr')
     const channel = getDefaultChannel(newVoiceState.guild);
     const gameName = newVoiceState.member.user.presence.activities.length && newVoiceState.member.user.presence.activities[0].name || 'their screen';
-    channel.send(`Hi <@${newVoiceState.member.user.id}> is streaming ${gameName}. 🎥`)
+    channel.send(`<@${newVoiceState.member.user.id}> is streaming ${gameName}. 🎥`)
   }
 });
 
